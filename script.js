8f14736902d91adfd91826c823d715612147ff42
kinya-njui <kinyanjuicharlesm@gmail.com>
2024-10-01 19:23:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 1 },
+    1024: { slidesPerView: 1 },
+  },
+});
